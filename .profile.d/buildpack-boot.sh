8e581c2a4bb7ed2d7bddc125d1c80b44959a26ba
Ademar Gonzalez <ademar.gonzalez@gmail.com>
2015-04-24 16:57:54 -0400

    set LD_LIBRARY_PATH

--- .profile.d/buildpack-boot.sh
+++ .profile.d/buildpack-boot.sh
@@ -1,3 +1,3 @@
-export PATH="$PATH:/app/vendor/mono/bin:/usr/local/bin:/usr/bin:/bin"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/vendor/mono/lib"
+export PATH="$PATH:/app/mono/bin:/usr/local/bin:/usr/bin:/bin"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/app/mono/lib"
 
